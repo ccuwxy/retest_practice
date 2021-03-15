980dcd386e7e9553b24f112b372ee182b0706922
WangXinyu <wxy990212@126.com>
2021-03-15 11:26:06 +0800

    practice

--- practice/07.c
+++ practice/07.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
+#include<string.h>
+/*
+int strlen(char* a)
+{
+    return 0;
+}*/
 
 int main()
 {
-	char a[20] = "cehiknqtw";
-	char s[] = "fbla";
-	int i, k, j;
-	for (k = 0; s[k] != '\0'; k++)
-	{
-		j = 0;
-		while (s[k] >= a[j] && a[j] != '\0')
-			j++;
-		for (i = strlen(a); i >= j; i--)
-			a[i + 1] = a[i];
-		a[j] = s[k];
+    
+    char a[20] = "cehiknqtw";
+    char s[] = "fbla";
+    int i, k, j;
+    printf("%d ", strlen(a));
+    for (k = 0; s[k] != '\0'; k++)
+    {
+        j = 0;
+        while (s[k] >= a[j] && a[j] != '\0')
+            j++;
+        for (i = strlen(a); i >= j; i--)
+            a[i + 1] = a[i];
+        a[j] = s[k];
 
-	}
-	puts(a);
-	return 0;
+    }
+    puts(a);
+    return 0;
 }
---
+++ practice/08.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void num()
+{
+	extern int x, y;
+	int a = 15, b = 10;
+	x = a - b;
+	y = a + b;
+}
+int x, y;
+
+int main()
+{
+	int a = 7, b = 5;
+	x = a + b;
+	y = a - b;
+	num();
+	printf("%d %d", x, y);
+	return 0;
+}
